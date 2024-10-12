249fd1ff2708b22dfd505a15a7334512643aa828
simplesumanth1 <105303952+simplesumanth1@users.noreply.github.com>
2024-10-12 22:35:23 +0530

    Time: 95 ms (55.44%) | Memory: 75.9 MB (64.40%) - LeetSync

--- 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.java
@@ -1,10 +1,14 @@
 class Solution {
     public int minGroups(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->(a[0]-b[0]));
+        // System.out.println("Intervals");
+        // for(int[]nums:intervals)
+        // for(int ele:nums)System.out.print(ele+ " ");
         PriorityQueue<Integer>pq = new PriorityQueue<>();
         for(int i=0;i<intervals.length;i++){
             int start = intervals[i][0];
             int end = intervals[i][1];
+            // System.out.println(pq);
             if(!pq.isEmpty() && pq.peek()<start)pq.poll();
             pq.offer(end);
         }
@@ -12,3 +16,4 @@ class Solution {
         
     }
 }
+// [[441459,446342],[801308,840640],[871890,963447],[228525,336985],[807945,946787],[479815,507766],[693292,944029],[751962,821744]]
